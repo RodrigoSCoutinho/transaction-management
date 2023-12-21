cb00d878bb3cfdebe6d23804ccbcf21418b9ab1e
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-12-21 10:56:47 -0300

    first commit

---
+++ src/test/java/br/com/rodrigoscoutino/transactionmanagement/TransactionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoscoutino.transactionmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
